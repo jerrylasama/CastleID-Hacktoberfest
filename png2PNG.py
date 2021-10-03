1c513ce8bc0bc8512274d278ec696279bbe476b3
Restyled.io <commits@restyled.io>
2021-10-03 08:48:04 +0000

    Restyled by autopep8

--- png2PNG.py
+++ png2PNG.py
@@ -7,11 +7,11 @@ path = './'
 for path, subdirs, files in os.walk(path):
     for f in files:
         curPath = os.path.join(path, f)
-        
+
         if not f.endswith('.png'):
             isFile = os.path.isfile(curPath)
             if isFile:
-                file = open(curPath,"rb+")
+                file = open(curPath, "rb+")
                 file.seek(1)
                 isPNG = file.read(3)
                 if str(isPNG) == "b'png'":
@@ -27,8 +27,7 @@ for path, subdirs, files in os.walk(path):
                 file.close()
                 # check again if the file successfuly converted then rename the file
                 if str(isPNG) == "b'PNG'":
-                    os.rename(curPath,curPath + ".png")
+                    os.rename(curPath, curPath + ".png")
                     print("[C] successfuly convert the file.")
         else:
             print("[F] " + f + " already converted.")
-
