9c5f13e17e90fae729b59aa7b05649839d9a5339
Restyled.io <commits@restyled.io>
2021-10-05 19:04:40 +0000

    Restyled by clang-format

--- coba.c
+++ coba.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int i;
-    int sum = 0x1234;
+  int i;
+  int sum = 0x1234;
 
-    sum += 0xABCDEF;
-    for (i=0; i < 10; i++) {
-        sum += i;
-    }
-    printf("sum=%d\n",sum);
-    sum = 0x2345;
+  sum += 0xABCDEF;
+  for (i = 0; i < 10; i++) {
+    sum += i;
+  }
+  printf("sum=%d\n", sum);
+  sum = 0x2345;
 }
